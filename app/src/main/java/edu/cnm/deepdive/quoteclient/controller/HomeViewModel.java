e576737256b6ea7b5baa20d05bc4a26155d14de6
Mariah Chavez-Graham <mgraham21@cnm.edu>
2020-03-09 15:19:07 -0600

    Added google sign in service

--- app/src/main/java/edu/cnm/deepdive/quoteclient/controller/HomeViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/quoteclient/controller/HomeViewModel.java
@@ -1,9 +1,11 @@
 package edu.cnm.deepdive.quoteclient.controller;
 
+import android.util.Log;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 import edu.cnm.deepdive.quoteclient.model.Quote;
+import edu.cnm.deepdive.quoteclient.service.GoogleSignInService;
 import edu.cnm.deepdive.quoteclient.service.QuoteRepository;
 import io.reactivex.disposables.CompositeDisposable;
 
@@ -31,13 +33,21 @@ public class HomeViewModel extends ViewModel {
   }
 
   public void refresh() {
-    pending.add(
-        repository.getRandom()
-            .subscribe(
-                quote::postValue,
-                throwable::postValue
-            )
-    );
+    GoogleSignInService.getInstance().refresh()
+        .addOnSuccessListener((account) -> {
+          Log.d(getClass().getName(), account.getDisplayName());
+          Log.d(getClass().getName(), account.getIdToken());
+          Log.d(getClass().getName(), account.getEmail());
+          Log.d(getClass().getName(), account.getId());
+          pending.add(
+              repository.getRandom(account.getIdToken())
+                  .subscribe(
+                      quote::postValue,
+                      throwable::postValue
+                  )
+          );
+        })
+        .addOnFailureListener(throwable::postValue);
   }
 
 }
